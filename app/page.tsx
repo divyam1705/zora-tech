8df7f5b40c777911806cc453a95cc49efca4687d
Divyam Arora <89800407+divyam1705@users.noreply.github.com>
2024-12-15 16:45:21 -0600

    logo ticker removal

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <div className="bg-white">
       <Hero />
-      <LogoTicker />
+      {/* <LogoTicker /> */}
       <Services ref={serviceRef} />
       <Testimonials />
     </div>
